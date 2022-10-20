9e5a39fa3de16b05ba620bdf9f7f3d857f2ca043
sjr0630 <75960352+ShinJongRock@users.noreply.github.com>
2022-10-20 09:53:53 +0900

    Add files via upload

---
+++ Rack/Components/SidebarData.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { styled, alpha } from "@mui/material/styles";
+import udttang from  "../img/udttang.png"
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom';
+
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto"
+  }
+}));
+
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch"
+    }
+  }
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+}));
+
+export const SidebarData = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+    
+    <AppBar position="static" color="inherit">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+
+          <Link to="/">
+            <img src={udttang} className ='log'/>
+          </Link>
+          </Typography>
+
+          <Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+
+          </Typography>
+         
+          <Button color="inherit" variant='outlined'>Login</Button>
+          &nbsp;&nbsp;
+          <Button color="inherit"  variant='outlined'>Logout</Button>
+        </Toolbar>
+    </AppBar>
+  </Box>
+  )
+}
