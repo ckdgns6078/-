fa29358411061beaa9e98692f5b4d1ca54353b38
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2022-10-21 10:04:05 +0900

    Add files via upload

--- Yeonji/App.js
+++ Yeonji/App.js
@@ -1,71 +1,28 @@
 import React from 'react';
+import SidebarData from './components/SidebarData';
 
-import Layout from './layouts/Layout';
-//import SignUpModal from './modals/SignUpModal';
-import {Container} from "react-bootstrap";
-//import KakaoLogout from './components/KakaoLogout';
-
-import "./App.css";
-import Auth from "./Auth";
-import Profile from "./Profile";
-import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
-import SignUpModal from './modals/SignUpModal';
-import SidebarData from '../src/components/SidebarData';
-
-
-function App(){
-  const REST_API_KEY = "1e3f63243c3b3f3d53a59048784908b6";
-  const REDIRECT_URI = "http://localhost:3001/oauth/kakao/callback";
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  //navererror();
+const App = () => {
   return (
     <>
+      <SidebarData/>
     
-    <SidebarData/>
-
-    {/* <Routes>
-    <Route path="/NaverLogin" element={<NaverLogin/>}></Route>
-    </Routes> */}
-    
-    
-   </>
+    </>
   );
-}
+};
 
 export default App;
 
 
-{/* <Layout>
-      <Container style={{ minHeight: "75vh" }}>
-        
-        </Container>
-      </Layout> */}
-      
-
-      <div className='App'>
-        {
-          sessionStorage.getItem("id")
-        }
-
-      </div>
-
-    {/* <SignUpModal></SignUpModal> */}
+// import AudioRecord from './voicerecode/AudioRecord';
+// import Sample from './voicerecode/Sample';
+// import Sample2 from './voicerecode/Sample2';
+// import Users from './components/Users';
+// <>
+  //   <AudioRecord></AudioRecord>
+  //   <Sample />
+  //   <Sample2 />
     
-    // <Router>
-    //   <div className="App">
-    //     <Routes>
-    //       <Route exact path="/" element={
-    //         <h1>
-    //           <a href={KAKAO_AUTH_URL}>Kakao Login</a>
-    //         </h1>}>
-    //       </Route>
-    //       <Route path="/oauth/kakao/callback" element={<Auth />}>
-            
-    //       </Route>
-    //       <Route path="/profile" element={<Profile />}>
-            
-    //       </Route>
-    //     </Routes>
-    //   </div>
-    // </Router>
-  //);
+  //   유저
+  //   <Users></Users>
+
+  // </>
