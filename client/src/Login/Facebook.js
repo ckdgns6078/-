8438791ebde1271c24481f2365c080313f5c5aac
yeonji123 <duathshdwkd1@naver.com>
2022-10-21 21:24:35 +0900

    Update Facebook.js

--- client/src/Login/Facebook.js
+++ client/src/Login/Facebook.js
@@ -2,62 +2,65 @@ import React, { Component } from "react";
 import FacebookLogin from "react-facebook-login";
 
 export default class Facebook extends Component {
-  state = {
-    isLoggedIn: false,
-    userID: "",
-    name: "",
-    email: "",
-    picture: ""
-  };
+    state = {
+        isLoggedIn: false,
+        userID: "",
+        name: "",
+        email: "",
+        picture: ""
+    };
 
-  responseFacebook = (response) => {
-    console.log(response);
-    if (response.status !== "unknown") {
-      this.setState({
-        isLoggedIn: true,
-        userID: response.userID,
-        name: response.name,
-        email: response.email,
-        picture: response.picture.data.url
-      });
+    responseFacebook = (response) => {
+        console.log(response);
+        if (response.status !== "unknown") {
+            this.setState({
+                isLoggedIn: true,
+                userID: response.userID,
+                name: response.name,
+                email: response.email,
+                picture: response.picture.data.url
+            });
+        }
+        sessionStorage.setItem("frontid", response.name);
+        sessionStorage.setItem("profile", response.picture.data.url);
+        sessionStorage.setItem("id", response.userID);
+
+        this.setState({
+            isLoggedIn: "",
+            userID: "",
+            name: "",
+            email: "",
+            picture: ""
+        })
+
+    };
+
+    componentClicked = () => {
+        console.log("clicked");
+    };
+
+    render() {
+        let fbContent;
+
+        fbContent = (
+            <FacebookLogin
+                appId="1480672229110640"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook}
+                icon="fa-facebook"
+            />
+        );
+        function clickFb() {
+
+        }
+
+        return (
+
+            <button id="facebookbutton" className="facebookbutton" onClick={fbContent}>
+                Login with Facebook
+            </button>
+        );
     }
-  };
-
-  componentClicked = () => {
-    console.log("clicked");
-  };
-
-  render() {
-    const { email, isLoggedIn, name, picture } = this.state;
-    let fbContent;
-
-    if (isLoggedIn) {
-      fbContent = (
-        <div
-          style={{
-            width: "400px",
-            margin: "auto",
-            background: "#f4f4f4",
-            padding: "20px"
-          }}
-        >
-          <img src={picture} alt={name} />
-          <h2>Welcome {name}</h2>
-          Email: {email}
-        </div>
-      );
-    } else {
-      fbContent = (
-        <FacebookLogin
-          appId="1480672229110640"
-          autoLoad={true}
-          fields="name,email,picture"
-          onClick={this.componentClicked}
-          callback={this.responseFacebook}
-          icon="fa-facebook"
-        />
-      );
-    }
-    return <div>{fbContent}</div>;
-  }
 }
